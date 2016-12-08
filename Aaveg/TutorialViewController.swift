e77354e2b3f4c4b1b7ac87d0716e7b44b1009ec1
Gautham Kumar <heygautham@gmail.com>
2016-12-08 22:22:02 +0530

    New method of implementing Page View

---
+++ Aaveg/TutorialViewController.swift
@@ -0,0 +1,54 @@
+//
+//  TutorialViewController.swift
+//  Aaveg
+//
+//  Created by Gautham Kumar on 08/12/16.
+//  Copyright © 2016 Gautham Kumar. All rights reserved.
+//
+
+class TutorialViewController: UIViewController {
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    var tutorialPageViewController: TutorialPageViewController? {
+        didSet {
+            tutorialPageViewController?.tutorialDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        pageControl.addTarget(self, action: #selector(TutorialViewController.didChangePageControlValue), for: .valueChanged)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let tutorialPageViewController = segue.destination as? TutorialPageViewController {
+            self.tutorialPageViewController = tutorialPageViewController
+        }
+    }
+    
+    
+    
+    /**
+     Fired when the user taps on the pageControl to change its current page.
+     */
+    func didChangePageControlValue() {
+        tutorialPageViewController?.scrollToViewController(index: pageControl.currentPage)
+    }
+}
+
+extension TutorialViewController: TutorialPageViewControllerDelegate {
+    
+    func tutorialPageViewController(_ tutorialPageViewController: TutorialPageViewController,
+                                    didUpdatePageCount count: Int) {
+        pageControl.numberOfPages = count
+    }
+    
+    func tutorialPageViewController(_ tutorialPageViewController: TutorialPageViewController,
+                                    didUpdatePageIndex index: Int) {
+        pageControl.currentPage = index
+    }
+    
+}
