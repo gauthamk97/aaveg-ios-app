b4f6ffbf2db775a471c23fa25633bdb5af71794c
Gautham Kumar <heygautham@gmail.com>
2016-12-13 16:09:47 +0530

    Initial work on blog

---
+++ Aaveg/BlogCard.swift
@@ -0,0 +1,22 @@
+//
+//  BlogCard.swift
+//  Aaveg
+//
+//  Created by Farina Balkish A on 13/12/16.
+//  Copyright © 2016 Gautham Kumar. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class BlogCard: UIView {
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
