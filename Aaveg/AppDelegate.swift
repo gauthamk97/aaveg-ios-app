3897dc52ecfd2d7bb127cc63a5835c217faafc0e
Gautham Kumar <heygautham@gmail.com>
2017-01-07 23:38:26 +0530

    Settings page functionality done

--- Aaveg/AppDelegate.swift
+++ Aaveg/AppDelegate.swift
@@ -18,6 +18,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let prefs = UserDefaults.standard
+        let shouldSkipHomeScreen = prefs.bool(forKey: "skipHomeScreen")
+        
+        if shouldSkipHomeScreen == true {
+            window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "revealVC")
+        }
+        
+        
         //Get scoreboard data
         obtainScoreboardData(index: 1)
         obtainScoreboardData(index: 2)
---
+++ Aaveg/SettingsViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SettingsViewController.swift
+//  Aaveg
+//
+//  Created by Gautham Kumar on 07/01/17.
+//  Copyright © 2017 Gautham Kumar. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var splashScreenSwitch: UISwitch!
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Settings"
+        
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        }
+        
+        self.splashScreenSwitch.addTarget(self, action: #selector(self.switchChange), for: UIControlEvents.valueChanged)
+        
+        UIApplication.shared.statusBarStyle = .default
+        
+        let prefs = UserDefaults.standard
+        let shouldSkipHomeScreen = prefs.bool(forKey: "skipHomeScreen")
+        
+        if shouldSkipHomeScreen {
+            DispatchQueue.main.async {
+                self.splashScreenSwitch.isOn = false
+                self.descriptionLabel.text = "Splash Screen will be skipped on App Launch"
+            }
+        }
+        
+        else {
+            DispatchQueue.main.async {
+                self.splashScreenSwitch.isOn = true
+                self.descriptionLabel.text = "Splash Screen will be displayed on App Launch"
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func switchChange() {
+        
+        let prefs = UserDefaults.standard
+        
+        if splashScreenSwitch.isOn {
+            prefs.set(false, forKey: "skipHomeScreen")
+            DispatchQueue.main.async {
+                self.descriptionLabel.text = "Splash Screen will be displayed on App Launch"
+            }
+            
+        }
+        
+        else {
+            prefs.set(true, forKey: "skipHomeScreen")
+            DispatchQueue.main.async {
+                self.descriptionLabel.text = "Splash Screen will be skipped on App Launch"
+            }
+        }
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
